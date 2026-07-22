c02cfd902c9338730da03a02634f0601392f1b01
nikooganesiani <niko.oganesiani@gmail.com>
2026-07-23 01:43:25 +0400

    Update generate_pages.js

--- generate_pages.js
+++ generate_pages.js
@@ -10,7 +10,7 @@ if (!fs.existsSync(productsDir)) {
 async function generatePages() {
     try {
         console.log('Скачиваем catalog.json с Cloudflare...');
-        const response = await fetch('https://catalog-api.niko-oganesiani.workers.dev/catalog.json');
+        const response = await fetch('https://catalog-api.niko-oganesiani.workers.dev/catalog');
         
         if (!response.ok) {
             throw new Error(`Ошибка загрузки: ${response.status}`);
