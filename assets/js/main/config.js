d898d9d63b42953e2436e6a76abe5e1b1a620a8a
nikooganesiani <niko.oganesiani@gmail.com>
2026-08-15 06:19:57 +0400

    Create config.js

---
+++ assets/js/main/config.js
@@ -0,0 +1,3 @@
+window.CATALOG_API_URL = window.CATALOG_API_URL || 'https://api.enkaelectronics.com.ge/catalog';
+window.WORKER_URL_TRACKING = window.WORKER_URL_TRACKING || 'https://delivery-modal.enkaelectronics.com.ge';
+window.WORKER_URL = window.WORKER_URL || 'https://form-handler.enkaelectronics.com.ge';
