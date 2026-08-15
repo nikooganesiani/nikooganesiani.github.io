1e06853296ddc3a83847b74be675e760a7c60345
nikooganesiani <niko.oganesiani@gmail.com>
2026-08-15 05:05:35 +0400

    Create tracking.js

---
+++ assets/js/product/tracking.js
@@ -0,0 +1,46 @@
+(function() {
+    const pDataset = document.getElementById('productData')?.dataset || {};
+    const sku = (pDataset.sku || "").trim();
+    const name = pDataset.name || "";
+    const rawPrice = pDataset.price || "";
+    const numericPrice = parseFloat(rawPrice.replace(/[^\d.]/g, '')) || 0;
+
+    if (sku) {
+        try {
+            if (typeof fbq === 'function') {
+                fbq('track', 'ViewContent', { content_ids: [sku], content_name: name, content_type: 'product', value: numericPrice, currency: 'GEL' });
+            }
+        } catch(e) {}
+    }
+
+    function waitForCapi(callback) {
+        if (typeof window.sendCapiEvent === 'function') {
+            callback();
+        } else {
+            var attempts = 0;
+            var interval = setInterval(function() {
+                attempts++;
+                if (typeof window.sendCapiEvent === 'function') {
+                    clearInterval(interval);
+                    callback();
+                } else if (attempts > 50) {
+                    clearInterval(interval);
+                }
+            }, 100);
+        }
+    }
+
+    waitForCapi(function() {
+        window.sendCapiEvent('PageView');
+
+        if (sku) {
+            window.sendCapiEvent('ViewContent', {
+                content_ids: [sku],
+                content_name: name,
+                content_type: 'product',
+                value: numericPrice,
+                currency: 'GEL'
+            });
+        }
+    });
+})();
